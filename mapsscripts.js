d079349c3eb53108ac5d7f398238e06c87db029b
NiamhS <sweeneyniamh@gmail.com>
2015-03-08 16:27:54 -0400

    Created for loop to pull latitude and longitude data from mappingData var created in mapsdata file

--- mapsscripts.js
+++ mapsscripts.js
@@ -5,7 +5,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Took code for adding a market from Leaflet; changed coordinates to my address in NY
-var marker = L.marker([40.814383,-73.959234]).addTo(map);
+var mappingDataArray = mappingData.rows
 
-marker.bindPopup("<b>Niamh's Apartment!</b><br>A popup to have a popup.").openPopup();
+for ( i = 0; i < mappingDataArray.length; i++) {
+	
+	var locationObject = mappingDataArray[i];
+	
+	// Took code for adding a market from Leaflet; changed coordinates to my address in NY
+	var marker = L.marker([locationObject.latitude, locationObject.longitude]).addTo(map);
+
+	marker.bindPopup("<b>Niamh's Apartment!</b><br>A popup to have a popup.").openPopup();
+}
